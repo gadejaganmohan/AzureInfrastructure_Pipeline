b07d11b9ea32c0e251dbfbf0bc15c7b944cee1e3
gadejaganmohan <gadejaganmohan@gmail.com>
2024-09-25 08:42:58 +0000

    test2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,18 @@ variable "resource_group_name" {
 variable "location" {
     default = ""
 }
+
+variable "client_id" {
+  description = "The Client ID of the Azure Service Principal"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The Client Secret of the Azure Service Principal"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID for the Azure Subscription"
+  type        = string
+}
